355c59397e1fd8d93d696c213195fce0be51bdec
Evan Relf <evan@evanrelf.com>
2020-07-16 20:57:58 -0700

    Polish `mean`, `median`, `mode`; give up on others

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,93 +1,74 @@
 use std::collections::HashMap;
 // use std::io::Read;
 
-fn mean(numbers: &Vec<i32>) -> Option<f32> {
+fn mean(numbers: &[i32]) -> Option<f32> {
     if numbers.is_empty() {
         return None;
     }
+
     let sum = numbers.iter().sum::<i32>() as f32;
-    let len = numbers.len() as f32;
-    Some(sum / len)
+    let length = numbers.len() as f32;
+    Some(sum / length)
 }
 
-fn median(numbers: &Vec<i32>) -> Option<i32> {
+fn median(numbers: &[i32]) -> Option<i32> {
     if numbers.is_empty() {
         return None;
     }
 
-    let len = numbers.len();
-    let mut numbers = numbers.clone();
-    numbers.sort();
+    let length = numbers.len();
+    let sorted_numbers = {
+        let mut vec = numbers.to_vec();
+        vec.sort();
+        vec
+    };
 
-    if len % 2 == 0 {
+    if length % 2 == 0 {
         // Even number of elements
-        let left_index = ((len as f32 / 2.0) - 1.0) as usize;
-        let right_index = (len as f32 / 2.0) as usize;
-        let left = numbers[left_index] as f32;
-        let right = numbers[right_index] as f32;
+        let left_index: usize = ((length as f32 / 2.0) - 1.0) as usize;
+        let right_index = (length as f32 / 2.0) as usize;
+        let left = sorted_numbers[left_index] as f32;
+        let right = sorted_numbers[right_index] as f32;
         let middle = (left + right / 2.0) as i32;
         Some(middle)
     } else {
         // Odd number of elements
-        let middle_index = (len as f32 / 2.0).floor() as usize;
-        let middle = numbers[middle_index];
+        let middle_index = (length as f32 / 2.0).floor() as usize;
+        let middle = sorted_numbers[middle_index];
         Some(middle)
     }
 }
 
-fn mode(numbers: &Vec<i32>) -> Option<i32> {
+fn mode(numbers: &[i32]) -> Option<i32> {
     let mut counts: HashMap<i32, i32> = HashMap::new();
 
-    for number in numbers {
+    for &number in numbers {
         counts
-            .entry(*number)
+            .entry(number)
             .and_modify(|count| *count += 1)
             .or_insert(1);
     }
 
     counts
         .iter()
-        .max_by_key(|(_, count)| *count)
-        .map(|(number, _)| *number)
+        .max_by_key(|(_, &count)| count)
+        .map(|(&number, _)| number)
 }
 
 // fn pig_latin(input: &str) -> String {
-//     if input.is_empty() {
-//         return String::new();
-//     }
-
-//     let head = todo!();
-//     let tail = todo!();
-
-//     if "aeiou".contains(head) {
-//         input.to_owned() + "-hay"
-//     } else {
-//         tail + "-" + head + "ay"
-//     }
 // }
 
 // fn employee_manager() {
-//     let mut employees: HashMap<String, String> = HashMap::new();
-
-//     loop {
-//         let mut input = String::new();
-
-//         std::io::stdin()
-//             .read_line(&mut input)
-//             .expect("Failed to read stdin")
-
-//         input = input.trim();
-//     }
 // }
 
 fn main() {
-    let numbers = vec![9, 2, 5, 6, 5, 6, 7, 6, 10];
+    let numbers = [9, 2, 5, 6, 5, 6, 7, 6, 10];
     println!("Numbers: {:?}", numbers);
     println!("Mean:    {:?}", mean(&numbers));
     println!("Median:  {:?}", median(&numbers));
     println!("Mode:    {:?}", mode(&numbers));
 
-    // let words = vec!["first", "apple"];
+    // let words = ["first", "apple"];
     // println!("Words:     {:?}", words);
     // println!("Pig latin: {:?}", words.iter().map(|word| pig_latin(word)));
 
